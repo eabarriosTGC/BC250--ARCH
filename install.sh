20f678cfdaabd1b30b2571a6d03823c7951cf7b2
LinuxeroGuajiro <121405061+eabarriosTGC@users.noreply.github.com>
2025-12-15 11:13:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,13 +8,16 @@
 # --- CONFIGURACIÓN DE DESCARGA ---
 RELEASE_URL="https://github.com/eabarriosTGC/BC250--ARCH/releases/download/v1.0"
 
-# Nombres de archivos
+# Nombres exactos de los archivos
+# NOTA: Los dos puntos ':' son importantes (es el 'epoch' de Arch Linux)
 KERNEL_PKG="linux-lts-amd-bc250-6.6.66-9-x86_64.pkg.tar.zst"
 KERNEL_HDR="linux-lts-amd-bc250-headers-6.6.66-9-x86_64.pkg.tar.zst"
-MESA_PKG="mesa-amd-bc250-24.3.1-8-x86_64.pkg.tar.zst"
-VULKAN_PKG="vulkan-radeon-amd-bc250-24.3.1-8-x86_64.pkg.tar.zst"
-LIB32_MESA_PKG="lib32-mesa-amd-bc250-24.3.1-1-x86_64.pkg.tar.zst"
-LIB32_VULKAN_PKG="lib32-vulkan-radeon-amd-bc250-24.3.1-1-x86_64.pkg.tar.zst"
+
+# Aquí se agregaron los "1:" antes de la versión 24.3.1
+MESA_PKG="mesa-amd-bc250-1:24.3.1-8-x86_64.pkg.tar.zst"
+VULKAN_PKG="vulkan-radeon-amd-bc250-1:24.3.1-8-x86_64.pkg.tar.zst"
+LIB32_MESA_PKG="lib32-mesa-amd-bc250-1:24.3.1-1-x86_64.pkg.tar.zst"
+LIB32_VULKAN_PKG="lib32-vulkan-radeon-amd-bc250-1:24.3.1-1-x86_64.pkg.tar.zst"
 # =================================================================================
 
 # Colores
